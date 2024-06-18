8d15f9347d6c4774fd539110f0c93980eda78a58
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-17 20:43:41 -0700

    Add project files.

---
+++ AbstractClassAssignment/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    public abstract class Person
+    {
+        //2 properties 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //method SayName()
+        public abstract void SayName();
+    }
+}
---
+++ AbstractClassAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Sam = new Employee() { firstName = "Sample", lastName = " Student" };
+            //instantiate employee object
+            Sam.SayName(); //call the sayname method 
+            Console.ReadLine();
+        }
+    }
+}
